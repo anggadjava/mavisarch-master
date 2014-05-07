ef3676009a99685e3aea8662b05caf1c85e1a5d9
anggadjava <anggadjava@gmail.com>
2014-05-08 00:06:22 +0700

    init git

--- application/views/content.php
+++
@@ -1,4 +0,0 @@
-<h3>Selamat Datang !!!</h3> 
-<h4><?php echo $this->session->userdata('nama');?> di <?php echo $this->config->item('nama_aplikasi');?> 
-<?php echo $this->config->item('nama_instansi');?>
-</h4>
